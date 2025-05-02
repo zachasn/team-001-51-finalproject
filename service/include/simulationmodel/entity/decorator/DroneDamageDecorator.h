01efa9850800917f0640abcc6ba258625b00625b
Michael VerBout <verbo032@umn.edu>
2025-05-02 11:48:09 -0500

    Create DroneDamageDecorator skeleton

---
+++ service/include/simulationmodel/entity/decorator/DroneDamageDecorator.h
@@ -0,0 +1,15 @@
+#ifndef DRONE_DAMAGE_DECORATOR_H
+#define DRONE_DAMAGE_DECORATOR_H
+
+#include "Drone.h"
+#include "DroneDecorator.h"
+#include "Package.h"
+
+class DroneDamageDecorator : public DroneDecorator {
+ private:
+  
+ public:
+  
+};
+
+#endif  // DRONE_DAMAGE_DECORATOR_H
